0fe43e01bfebf333d2365c086eaa8ff7cab482f5
Emil Flodmark <emil.flodmark@gmail.com>
2024-09-26 13:32:58 +0200

    testa

--- moment1/www/public/evaluate.php
+++ moment1/www/public/evaluate.php
@@ -10,18 +10,21 @@
      	$ans2 = $_POST['q2'];
         $ans3 = $_POST['q3'];
         $ans4 = $_POST['q4'];
+        $ans5 = $_POST['q5'];
         $points = 0;
      	
-     	if($ans1 == 1789)
+     	if($ans1 == 9)
            $points++;
-     	if($ans2 == 17)
+     	if($ans2 == 15)
            $points++;
-        if($ans3 == "nej")
+        if($ans3 == 49)
            $points++;
         if($ans4 == 3)
            $points++;
+        if($ans5 == 84)
+           $points++;
      	echo("<p>Ditt namn: $name1</p>");
-        echo("<p>Du fick " . $points . " av 4 möjliga</p>");
+        echo("<p>Du fick " . $points . " av 5 möjliga</p>");
 ?>
 </body>
 </html>
